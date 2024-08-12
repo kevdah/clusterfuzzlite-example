0f33021589fc42fce0e56ae884b5c34731e5c9ae
kevdah <kevin.dahlen@gmail.com>
2024-08-12 12:33:15 +0200

    Reintroduced bug

--- my_api.cpp
+++ my_api.cpp
@@ -19,7 +19,7 @@ size_t DoStuff(const std::string &str) {
     Idx++;
   if (str.find("ouch") != std::string::npos)
     Idx++;
-  if (str.find("omg") != std::string::npos && Idx <= 4)
+  if (str.find("omg") != std::string::npos)
     Idx++;
   return Vec[Idx];
 }
